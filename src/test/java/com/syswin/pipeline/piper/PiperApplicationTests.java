c84fa68b16c1c3553a73d2342403950cac920b08
韦宏毅 <weihongyi@syswin.com>
2019-02-27 13:59:10 +0800

    Initial commit

---
+++ src/test/java/com/syswin/pipeline/piper/PiperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.syswin.pipeline.piper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PiperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
