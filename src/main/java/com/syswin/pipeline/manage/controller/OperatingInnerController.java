c887298d478e4a6a5a0c4dc6df1320c00a0a3161
罗洪州 <luohongzhou@syswin.com>
2019-03-30 22:11:14 +0800

    1、调整 后台Admin

--- src/main/java/com/syswin/pipeline/manage/controller/OperatingInnerController.java
+++ src/main/java/com/syswin/pipeline/manage/controller/OperatingInnerController.java
@@ -1,17 +1,17 @@
 package com.syswin.pipeline.manage.controller;
 
-import com.syswin.pipeline.db.model.Account;
+import com.syswin.pipeline.db.model.Userlog;
 import io.swagger.annotations.Api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * 发券
+ * 操作记录
  * Created by 115477 on 2018/11/28.
  */
 @Controller
-@RequestMapping("/manage/account")
-@Api(value = "account", tags = "account")
-public class AccountInnerController extends BaseAction<Account> {
+@RequestMapping("/manage/userlog")
+@Api(value = "operating", tags = "operating")
+public class OperatingInnerController extends BaseAction<Userlog> {
 
 }
--- src/main/java/com/syswin/pipeline/manage/controller/UserlogInnerController.java
+++ src/main/java/com/syswin/pipeline/manage/controller/UserlogInnerController.java
@@ -1,16 +1,17 @@
 package com.syswin.pipeline.manage.controller;
 
 import com.syswin.pipeline.db.model.Userlog;
+import io.swagger.annotations.Api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * 发券
+ * 使用日志
  * Created by 115477 on 2018/11/28.
  */
 @Controller
 @RequestMapping("/manage/userlog")
-//@Api(value = "userlog", tags = "userlog")
+@Api(value = "userlog", tags = "userlog")
 public class UserlogInnerController extends BaseAction<Userlog> {
 
 }
