0b8d42f659de15f10620852dca7d22ce8178c630
罗洪州 <luohongzhou@syswin.com>
2019-04-15 11:34:40 +0800

    修改菜单

--- src/main/java/com/syswin/pipeline/db/repository/DeviceInfoRepository.java
+++ src/main/java/com/syswin/pipeline/db/repository/DeviceInfoRepository.java
@@ -2,27 +2,31 @@ package com.syswin.pipeline.db.repository;
 
 import com.syswin.pipeline.db.model.DeviceInfo;
 import com.syswin.pipeline.db.model.DeviceInfoExample;
+
 import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DeviceInfoRepository {
-    long countByExample(DeviceInfoExample example);
+	long countByExample(DeviceInfoExample example);
 
-    int deleteByPrimaryKey(Integer id);
+	int deleteByPrimaryKey(Integer id);
 
-    int insert(DeviceInfo record);
+	int insert(DeviceInfo record);
 
-    int insertSelective(DeviceInfo record);
+	int insertSelective(DeviceInfo record);
 
-    List<DeviceInfo> selectByExample(DeviceInfoExample example);
+	List<DeviceInfo> selectByExample(DeviceInfoExample example);
 
-    DeviceInfo selectByPrimaryKey(Integer id);
+	DeviceInfo selectByPrimaryKey(Integer id);
 
-    int updateByExampleSelective(@Param("record") DeviceInfo record, @Param("example") DeviceInfoExample example);
+	int updateByExampleSelective(@Param("record") DeviceInfo record, @Param("example") DeviceInfoExample example);
 
-    int updateByExample(@Param("record") DeviceInfo record, @Param("example") DeviceInfoExample example);
+	int updateByExample(@Param("record") DeviceInfo record, @Param("example") DeviceInfoExample example);
 
-    int updateByPrimaryKeySelective(DeviceInfo record);
+	int updateByPrimaryKeySelective(DeviceInfo record);
 
-    int updateByPrimaryKey(DeviceInfo record);
+	int updateByPrimaryKey(DeviceInfo record);
 }
