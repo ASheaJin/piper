f7d8be77e5d0a880be483c26adbecd99a7655f04
罗洪州 <luohongzhou@syswin.com>
2019-04-18 09:32:12 +0800

    七牛云支持登录

--- start.sh
+++ start.sh
@@ -8,6 +8,6 @@ ps -ef|grep "piper-1.5.2-SNAPSHOT.jar" | grep java | awk '{print $2}' | xargs ki
 #打包
 /usr/maven/bin/mvn -U package
 #重启
-nohup java -Xdebug -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=8889 -jar target/piper-1.5.15-SNAPSHOT.jar -Dspring.profiles.active=dev> pipeline-api.log 2>&1 &
+nohup java -Xdebug -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=8889 -jar target/piper-1.6.1-SNAPSHOT.jar -Dspring.profiles.active=dev> pipeline-api.log 2>&1 &
 #打印日志
 tail -f pipeline-api.log
