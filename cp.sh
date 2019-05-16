ae1ddd91e913b5c41bf97afbccda33d25c7d86a4
root <root@BG-docker43.107>
2019-05-16 09:46:45 +0800

    修改前端web

--- cp.sh
+++ cp.sh
@@ -5,8 +5,8 @@ git fetch
 git pull 
 
 #删除并拷贝文件
-rm -rf /src/main/resources/public/web 
-cp -a  web /src/main/resources/public/web
+rm -rf src/main/resources/public/web 
+cp -a  web src/main/resources/public/web
 
 #git提交
 git add .
