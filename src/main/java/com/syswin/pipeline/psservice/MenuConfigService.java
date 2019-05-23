1c00564f4491b7c84defa0af2beb756a96d036ed
罗洪州 <luohongzhou@syswin.com>
2019-05-23 16:20:13 +0800

    调通 齐定 接入PS项目

--- src/main/java/com/syswin/pipeline/psservice/MenuConfigService.java
+++ src/main/java/com/syswin/pipeline/psservice/MenuConfigService.java
@@ -20,7 +20,7 @@ public class MenuConfigService implements IMenuConfigService {
 		MsgHeader msgHeader = PsClientKeeper.msgHeader();
 		logger.info("msgHeader" + msgHeader.toString());
 		//根据访问者的权限配置菜单 msgHeader 里面有用户的 信息
-		List<String> aas = Arrays.asList(getKey(accountNo+1,"1"),getKey(accountNo+2,"2"));
+		List<String> aas = Arrays.asList(getKey(accountNo,"1"),getKey(accountNo,"2"));
 		logger.info("aas" + aas.toString());
 		return aas;
 
