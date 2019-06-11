86b3337df8a072436bc97a353bdd7a223f54d9c6
韦宏毅 <weihongyi@syswin.com>
2019-06-11 15:59:09 +0800

    补充索引

--- doc/db/20190611/addIndex.sql
+++ doc/db/20190611/addIndex.sql
@@ -6,3 +6,12 @@ ADD INDEX `publisher_id` (`publisher_id` ASC);
 ALTER TABLE `subscription`
 ADD INDEX `user_id` (`user_id` ASC),
 ADD INDEX `publisher_id` (`publisher_id` ASC);
+
+ALTER TABLE `publisher`
+ADD INDEX `user_id` (`user_id` ASC);
+
+
+use temail_piper;
+
+ALTER TABLE `consumer`
+ADD INDEX `user_id` (`user_id` ASC);
